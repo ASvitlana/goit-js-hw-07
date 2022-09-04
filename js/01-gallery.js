d578888847d8155ecafbd169cf323f64844e93f8
Светлана Петровна Андрела <104157616+ASvitlana@users.noreply.github.com>
2022-09-04 13:06:58 +0300

    ДЗ-7, завдання 1, 2

---
+++ js/01-gallery.js
@@ -0,0 +1,50 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+
+const galleryContainer = document.querySelector('.gallery');
+const imagesMarkup = createGalleryItemsEl(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', imagesMarkup);
+
+function createGalleryItemsEl(galleryItems) {
+    return galleryItems.map(({ preview,  original}) => {
+        return `
+        <div class="gallery__item">
+            <a class="gallery__link" href="large-image.jpg">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="Image description"
+                />
+            </a>
+        </div>
+        `;
+    })
+    .join('');
+}
+
+galleryContainer.addEventListener('click', (event) => {
+    event.preventDefault();
+    
+    if(event.target.nodeName !== 'IMG') {
+        return;
+    };
+    
+    const selectedImage = event.target.getAttribute('data-source');
+
+    const instance = basicLightbox.create(`
+        <img src="${selectedImage}" width = '800'>
+    `);
+
+    instance.show();
+
+
+    galleryContainer.addEventListener('keydown', event => {
+		if (event.key === 'Escape') {
+			instance.close();
+		}
+	})
+} );
+
